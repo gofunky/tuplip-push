6f24bde0b934ef2aab22ab5f1c943962285919e5
matfax <matthias.fax@gmail.com>
2020-08-16 20:30:19 +0200

    feat: make docker build optional

--- README.md
+++ README.md
@@ -2,3 +2,52 @@
 [GitHub Action](https://github.com/features/actions) that uses [tuplip](https://github.com/gofunky/tuplip) to build and push a single given Dockerfile 
 to the Docker hub.
 
+## Examples
+
+### tuplip build
+
+This is a typical example for a pull request workflow - having a Dockerfile in the root of the repository.
+
+```yaml
+name: build
+on:
+  pull_request:
+    branches: [ master ]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+    - uses: satackey/action-docker-layer-caching@master
+    - name: build docker image
+      uses: gofunky/tuplip-push@master
+      with:
+        buildOnly: true
+        rootVersion: 'latest'
+        exclusiveLatest: true
+```
+
+### tuplip push latest
+
+This is a typical example for a master branch push workflow - having a Dockerfile in the root of the repository.
+
+```yaml
+name: build
+on:
+  pull_request:
+    branches: [ master ]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+    - uses: satackey/action-docker-layer-caching@master
+    - name: build docker image
+      uses: gofunky/tuplip-push@master
+      with:
+        buildOnly: true
+        rootVersion: 'latest'
+        exclusiveLatest: true
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,54 +1,32 @@
 export TAGS=""
-export PUSH_TO=""
 export REPOSITORY=""
 export BUILD_PUSH="push"
-export EXCLUDE_MAJOR=""
-export EXCLUDE_MINOR=""
-export EXCLUDE_BASE=""
-export ADD_LATEST=""
-export EXCLUSIVE_LATEST=""
 export STRAIGHT=""
-export ROOT_VERSION_FLAG=""
-export ROOT_VERSION=""
-export FILTER_FLAG=""
-export FILTER=""
+export VERSION=""
+export SOURCE="$GITHUB_WORKFLOW"
 
-if [ -n "$INPUT_EXCLUDEMAJOR" ]; then
-  EXCLUDE_MAJOR="--exclude-major"
-fi
-
-if [ -n "$INPUT_EXCLUDEMINOR" ]; then
-  EXCLUDE_MINOR="--exclude-minor"
-fi
-
-if [ -n "$INPUT_EXCLUDEBASE" ]; then
-  EXCLUDE_BASE="--exclude-base"
-fi
-
-if [ -n "$INPUT_ADDLATEST" ]; then
-  ADD_LATEST="--add-latest"
-fi
-
-if [ -n "$INPUT_EXCLUSIVELATEST" ]; then
-  EXCLUSIVE_LATEST="--exclusive-latest"
-fi
+# TODO Caching (attach entrypoint internal to worker external cache somehow)
 
 if [ -n "$INPUT_ROOTVERSION" ]; then
-  ROOT_VERSION_FLAG="--root-version"
-  ROOT_VERSION="$INPUT_ROOTVERSION"
+  VERSION="$INPUT_ROOTVERSION"
 fi
 
-if [ -n "$INPUT_FILTER" ]; then
-  FILTER_FLAG="--filter"
-  FILTER="$INPUT_FILTER"
+if [ -n "$INPUT_SOURCETAG" ]; then
+  SOURCE="$INPUT_SOURCETAG"
+  echo "Skipping docker build..."
+else
+  echo "Executing docker build..."
+  docker build -t "$SOURCE" -f "$INPUT_DOCKERFILE" ${VERSION:+--build-arg VERSION} "$INPUT_PATH"
+  # TODO More build args
 fi
 
 if [ -n "$INPUT_BUILDONLY" ]; then
   BUILD_PUSH="build"
+  SOURCE=""
+  echo "Executing tuplip build..."
 else
 
   if ! grep -q "ARG REPOSITORY=" "$INPUT_PATH/$INPUT_DOCKERFILE"; then
-    PUSH_TO="to"
     REPOSITORY="$INPUT_REPOSITORY"
   fi
 
@@ -64,12 +42,14 @@ else
     STRAIGHT="--straight"
   fi
 
+  echo "Executing tuplip push..."
 fi
 
 TAGS=$(
-  /usr/local/bin/tuplip $BUILD_PUSH $PUSH_TO "$REPOSITORY" from file "$INPUT_PATH/$INPUT_DOCKERFILE" \
-  --verbose $EXCLUDE_MAJOR $EXCLUDE_MINOR $EXCLUDE_BASE $ADD_LATEST $EXCLUSIVE_LATEST $STRAIGHT \
-  $ROOT_VERSION_FLAG "$ROOT_VERSION" $FILTER_FLAG "$FILTER"
+  /usr/local/bin/tuplip $BUILD_PUSH ${REPOSITORY:+to "$REPOSITORY"} from file "$INPUT_PATH/$INPUT_DOCKERFILE" \
+  --verbose ${INPUT_EXCLUDEMAJOR:+--exclude-major} ${INPUT_EXCLUDEMINOR:+--exclude-minor} \
+  ${INPUT_EXCLUDEBASE:+--exclude-base} ${INPUT_ADDLATEST:+--add-latest} ${INPUT_EXCLUSIVELATEST:+--exclusive-latest} \
+  $STRAIGHT ${VERSION:+--root-version "$VERSION"} ${INPUT_FILTER:+--filter "$INPUT_FILTER"}
 )
 
 echo "::set-output tags:: $TAGS"
