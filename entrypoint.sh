ec990e7f1c68769db2e4a37bbe32862bf3bb8af9
matfax <matthias.fax@gmail.com>
2020-08-20 11:02:00 +0200

    fix: syntax of shell break lines

--- entrypoint.sh
+++ entrypoint.sh
@@ -89,11 +89,11 @@ fi
 
 echo "Executing tuplip $BUILD_PUSH..."
 
-TAGS=$(
-  tuplip $BUILD_PUSH ${REPOSITORY:+to "$REPOSITORY"} from file "$INPUT_PATH/$INPUT_DOCKERFILE" \
+TAGS=$( \
+  /usr/local/bin/tuplip $BUILD_PUSH ${REPOSITORY:+to "$REPOSITORY"} from file "$INPUT_PATH/$INPUT_DOCKERFILE" \
   --verbose ${INPUT_EXCLUDEMAJOR:+--exclude-major} ${INPUT_EXCLUDEMINOR:+--exclude-minor} \
   ${INPUT_EXCLUDEBASE:+--exclude-base} ${INPUT_ADDLATEST:+--add-latest} ${INPUT_EXCLUSIVELATEST:+--exclusive-latest} \
-  $STRAIGHT ${VERSION:+--root-version "$VERSION"} ${FILTER:+--filter "$FILTER"}
+  $STRAIGHT ${VERSION:+--root-version "$VERSION"} ${FILTER:+--filter "$FILTER"} \
 )
 
 echo "::set-output name=tags:: $TAGS"
